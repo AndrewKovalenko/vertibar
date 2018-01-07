5793e41838babff3dcf732448f95cc1707910401
Andrew Kovalenko <mr.kovalenko.a.o@gmail.com>
2018-01-06 18:39:22 -0800

    add module build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'vertibar.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/umd'),
     library: 'v',
     libraryTarget: 'umd',
   },
