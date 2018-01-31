8b8461dfa68e0e908da5cef22bf8099b53bda2b1
Andrew Kovalenko <akovalenko@intuit.com>
2018-01-30 19:02:49 -0800

    add Function throttle

--- src/extensions/function.js
+++ src/extensions/function.js
@@ -16,3 +16,22 @@ Function.prototype.debounce =
       timeoutId = setTimeout(() => self.apply(callContext, callArguments), wait);
     };
   };
+
+Function.prototype.throttle =
+  Function.prototype.throttle || function throttle(wait, context) {
+    if (typeof wait === 'undefined') {
+      throw new Error('Missing debounce waiting period.');
+    }
+
+    let throttled;
+    const self = this;
+
+    return (...callArguments) => {
+      const callContext = context || this;
+
+      if (!throttled) {
+        self.apply(callContext, callArguments);
+        setTimeout(() => { throttled = false; }, wait);
+      }
+    };
+  };
