6bf4b69bc98899048e5a1e14046ee426e9fcf131
Andrew Kovalenko <akovalenko@intuit.com>
2018-02-05 11:09:27 -0800

    finish Function throttle

--- tests/function.test.js
+++ tests/function.test.js
@@ -13,7 +13,7 @@ test.before(() => {
   fakeClock = useFakeTimers();
 });
 
-const makeCalls = async (mockFunction, mockCallArguments) => {
+const makeCalls = async (mockFunction, mockCallArguments, callRepeatingIntervals) => {
   const result = new Promise((resolve) => {
     let callsAttempts = 0;
 
@@ -26,7 +26,7 @@ const makeCalls = async (mockFunction, mockCallArguments) => {
         fakeClock.tick(THROTTLE_DELAY + LITTLE_BIT_LATER);
         resolve();
       }
-    }, CALL_REPEATING_INTERVAL);
+    }, callRepeatingIntervals);
   });
 
   fakeClock.tick((NUMBER_OF_DESIRED_CALLS * CALL_REPEATING_INTERVAL) + LITTLE_BIT_LATER);
@@ -38,8 +38,8 @@ test('Function debounce', async (t) => {
   const mockFunction = spy();
   const mockContext = {};
   const mockCallArguments = [1, 2, 3];
-  const throtteledMock = mockFunction.debounce(THROTTLE_DELAY, mockContext);
-  await makeCalls(throtteledMock, mockCallArguments);
+  const debouncedMock = mockFunction.debounce(THROTTLE_DELAY, mockContext);
+  await makeCalls(debouncedMock, mockCallArguments, CALL_REPEATING_INTERVAL);
 
   t.true(mockFunction.calledOnce);
   t.true(mockFunction.calledWith(...mockCallArguments));
@@ -50,22 +50,58 @@ test('Function debounce uses initial caller context by default', async (t) => {
   const mockFunction = spy();
   const mockContext = {};
   const mockCallArguments = [1, 2, 3];
-  const throtteledMock = mockFunction.debounce(THROTTLE_DELAY);
+  const debouncedMock = mockFunction.debounce(THROTTLE_DELAY);
   const artificialContextCall = makeCalls.bind(mockContext);
-  await artificialContextCall(throtteledMock, mockCallArguments);
+  await artificialContextCall(debouncedMock, mockCallArguments, CALL_REPEATING_INTERVAL);
 
   t.true(mockFunction.calledOnce);
   t.true(mockFunction.calledWith(...mockCallArguments));
   t.true(mockFunction.calledOn(mockFunction));
 });
 
-test('Function debounce uses initial caller context by default', async (t) => {
+test('Function debounce throws an error if there is no waiting parameter passed', async (t) => {
   const mockFunction = spy();
   const error = t.throws(() => {
     mockFunction.debounce();
   }, Error);
 
   t.is(error.message, 'Missing debounce waiting period.');
+});
+
+test('Function throttle', async (t) => {
+  const mockFunction = spy();
+  const mockContext = {};
+  const mockCallArguments = [1, 2, 3];
+  const DELAY_FOR_TWO_CALLS = THROTTLE_DELAY / 2;
+  const throttleMock = mockFunction.throttle(DELAY_FOR_TWO_CALLS, mockContext);
+  await makeCalls(throttleMock, mockCallArguments, CALL_REPEATING_INTERVAL);
+
+  t.is(mockFunction.callCount, 2, 'Function was not throtteled as expected');
+  t.true(mockFunction.calledWith(...mockCallArguments));
+  t.true(mockFunction.calledOn(mockContext));
+});
+
+
+test('Function throttle uses initial caller context by default', async (t) => {
+  const mockFunction = spy();
+  const mockContext = {};
+  const mockCallArguments = [1, 2, 3];
+  const throttleMock = mockFunction.throttle(THROTTLE_DELAY);
+  const artificialContextCall = makeCalls.bind(mockContext);
+  await artificialContextCall(throttleMock, mockCallArguments, CALL_REPEATING_INTERVAL);
+
+  t.true(mockFunction.calledOnce);
+  t.true(mockFunction.calledWith(...mockCallArguments));
+  t.true(mockFunction.calledOn(mockFunction));
+});
+
+test('Function throttle throws an error if there is no waiting parameter passed', async (t) => {
+  const mockFunction = spy();
+  const error = t.throws(() => {
+    mockFunction.throttle();
+  }, Error);
+
+  t.is(error.message, 'Missing throttle waiting period.');
 });
 
 test.after(() => {
